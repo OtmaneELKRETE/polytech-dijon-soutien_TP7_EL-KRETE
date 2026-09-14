6fad2204b5968152798b48b250861ba36fbc2752
oe543944 <117648188+Otakasama@users.noreply.github.com>
2026-09-14 09:27:28 +0200

    Gestion De la jouabilité et la possibilité de relance des partie et mise en place archi pour les autres possibilitées

--- MasterMind.py
+++ MasterMind.py
@@ -12,6 +12,40 @@ class MasterMind:
     def __init__(self):
         self.code = Code.Code(4,2)
         self.tentative = Tentative.Tentative()
+
+    """
+    Gestion du jeu
+    """
+    def jouabilite(self):
+        print("1. Jouer") 
+        print("2. Remettre à zéro les statistiques") 
+        print("3. Quitter")
+
+        valeur = self.verifSaisie()
+
+        if valeur == 1:
+            while valeur == 1:
+                self.resetParam()
+                ## Affichage sauvegarde
+                self.partieMasterMind()
+                print("1. Rejouer") 
+                print("2. Remettre à zéro les statistiques") 
+                print("3. Quitter")
+
+                valeur = self.verifSaisie()
+        elif valeur == 2:
+            ## Gestion logique suppression fichier statistique (le vider)
+            pass
+        else:
+            ## Gestion logique sauvegarde
+            pass
+
+    """
+    Permet de réinitialiser les paramètres
+    """
+    def resetParam(self):
+        self.code = Code.Code(4,2)
+        self.tentative = Tentative.Tentative()
     """
     Déroulement d'une partie MasterMin
     """
@@ -27,4 +61,14 @@ class MasterMind:
 
         self.tentative.gestionDéfaite(self.code,i)
 
-MasterMind().partieMasterMind()
+    """
+    Permet de vérifier les saisies 
+    """
+    def verifSaisie(self):
+        try:
+            valeur = int(input("Faites votre choix en séléctionnant 1 2 ou 3 : "))
+            return valeur
+        except ValueError:
+            print("Erreur de saisie, vous avez saisit une valeur invalide")
+
+MasterMind().jouabilite()
